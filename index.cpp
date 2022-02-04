8be3722ec48fbb8fff363647d583cf90302b2804
sohammanjrekar <mrsohammanjrekar@gmail.com>
2022-02-04 20:55:37 +0530

    update

---
+++ index.cpp
@@ -0,0 +1,64 @@
+
+#include <iostream> 
+ //preprocessor directive used in include files
+
+ #include <stdio.h>
+using namespace std;
+
+int main(){  //the execution of code begins from main function 
+    cout<<"hello\n";
+// int a ; //declaration
+// a=56; //initaization
+
+//camelcase notation
+// int marksInMaths=2;
+// cout<<marksInMaths;
+// short a;
+// int b;
+// double c;
+// long d;
+// long long e;
+// cout<<sizeof(a)<<endl;
+// cout<<sizeof(b)<<endl;
+// cout<<sizeof(c)<<endl;
+// cout<<sizeof(d)<<endl;
+// cout<<sizeof(e)<<endl;
+// float h=1150.2364758;
+// long double g=1112.4567329832;
+// cout<<h<<endl;
+// cout<<g<<endl;
+// cout<<sizeof(h)<<endl;
+// cout<<sizeof(g)<<endl;
+
+// const int score=445;  //constant create
+
+int a,b,c;
+cin>>a;
+// if(a%2==0){
+// cout<<"even";
+// }
+// else{
+// cout<<"odd";
+// }
+
+// continue  = skip to the next iteration  of the loop 
+//break = termnate the loop
+// for(int i=0;i<a;i++)
+// {
+//     if(i%3==0){
+//         continue;
+//     }
+//     cout<<i<<endl;
+// }
+
+
+for(int i= 2; i<a; i++){
+    if(a%i==0){
+        
+    }
+}
+
+
+
+    return 0;
+}
